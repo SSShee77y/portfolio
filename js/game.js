f7133fc4de6832e5966ee4498a87c904afa58ff7
Leo Chen <46356943+SSShee77y@users.noreply.github.com>
2025-01-01 17:05:50 -0500

    Slight tweak on boids

--- js/game.js
+++ js/game.js
@@ -12,20 +12,31 @@ const navDiv = document.getElementById("nav");
 const gameCanvasHeight = homeDiv.offsetHeight + navDiv.offsetHeight;
 let canvasElement = null;
 
-function setup() {
+// Mouse reticle
+const reticle = document.getElementById("reticle");
+let reticleHover = false;
 
+function setup() {
+    // Game canvas
     let gameCanvas = createCanvas(windowWidth, gameCanvasHeight);
     gameCanvas.id('p5-canvas');
-
     canvasElement = document.getElementById('p5-canvas');
 
+    // Evader settings
     evaderVelocityFactor = Math.min(2.5, width/1200 + 3);
     evaderDetectionRange = Math.min(500, width * height / 11000 + 100);
+
+    // Initial Reticle
+    mouseX = windowWidth/2;
+    mouseY = windowHeight/2 - 20;
+    reticle.style.transform = `translate(${mouseX - evaderDetectionRange/2}px, ${mouseY - evaderDetectionRange/2 + window.scrollY}px)` + (reticleHover ? ` scale(.93)` : ``);
+    reticle.style.width = `${evaderDetectionRange}px`;
+    reticle.style.height = `${evaderDetectionRange}px`;
         
     // Create some Evaders with random positions and velocity
-    let spawnLimit = Math.min(400, width * height / 6000 + 60);
+    let spawnLimit = Math.min(450, width * height / 5000 + 80);
     for (let i = 0; i < spawnLimit; i++) {
-        let randomPos = createVector(random(width), random(height));
+        let randomPos = createVector(random(-evaderDetectionRange/2, width+evaderDetectionRange/2), random(-evaderDetectionRange/2, height+evaderDetectionRange/2));
         let randomVel = p5.Vector.random2D().setMag(evaderVelocityFactor);
         evaders.push(new Evader(randomPos, randomVel));
     }
@@ -41,10 +52,6 @@ function windowResized() {
     evaderVelocityFactor = Math.min(10, width/1200 + 3);
     evaderDetectionRange = Math.min(500, width * height / 11000 + 100);
 }
-
-// Mouse reticle
-const reticle = document.getElementById("reticle");
-let reticleHover = false;
 
 document.addEventListener('mousemove', (e) => {
     if (window.scrollY > gameCanvasHeight + evaderDetectionRange / 2) {
